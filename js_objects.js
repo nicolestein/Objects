f47f1ec944947bbac5c44f944c2ef609cba754db
Nicole I Stein <ns60549@srcs.loc>
2018-01-24 12:15:23 -0800

    work

---
+++ js_objects.js
@@ -0,0 +1,33 @@
+function Cube(length,mass) {
+    this.length = length;
+    this.mass = mass;
+    this.volume = Math.pow(length,3);
+    this.density = this.mass/this.volume;
+    this.surfaceArea = Math.power(length,2)*6;
+}
+
+function Person(firstname,lastname,bithdate) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.birthdate = birthdate;
+
+}
+
+function Pet(name,species,sleep,hunger,awake) {
+    this.name = name;
+    this.species = species;
+    this.sleep =
+    this.speak = speak;
+}
+
+    function speak () {
+        if (this.type === "Dog") {
+            return "Woof! That means I love you!";
+        } else if (this.type === "Cat") {
+            return "Ugh. meow";
+        } else {
+            return "";
+        }
+    }
+
+    funstion
